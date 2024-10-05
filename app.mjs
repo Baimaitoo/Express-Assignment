90af2ac3e5bde43e5817d4e7a1631a3206c75c6c
Suwinai <suwinai.apionline@gmail.com>
2024-10-05 22:06:09 +0700

    test API

--- app.mjs
+++ app.mjs
@@ -1,17 +1,20 @@
 import express from "express";
+import req from "express/lib/request";
 
 const app = express();
 const port = 4000;
 
-app.get("/profiles", (req, res) => {
-    return res.json({
-        "data":  {
-            "name": "john",
-            "age": 20
-        }
-    });
+app.get("test", (req, res) => {
+    return res.json( "Server API is working");
 }) ;
 
+
+app.post("/posts", (req, res) => {
+    // ลอจิกในการเก็บข้อมูลของโพสต์ลงในฐานข้อมูล
+return res.status(201).json({
+    message: "Created post successfully",
+});
+});
 app.listen(port, () =>{
     console.log(`Server is running at ${port}`);
 
